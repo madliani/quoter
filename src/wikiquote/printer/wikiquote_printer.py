b4d5d1dc94787c10f9e6cd7c90668ecff8c50cc9
madliani <madliani@hotmail.com>
2024-10-13 18:54:51 +0400

    refactor: rename author printer methods

--- src/wikiquote/printer/wikiquote_printer.py
+++ src/wikiquote/printer/wikiquote_printer.py
@@ -5,11 +5,11 @@ from wikiquote.printer.quote_printer import QuotePrinter
 class WikiquotePrinter:
     def print_pages(self, pages_scraper):
         author_printer = AuthorPrinter(pages_scraper)
-        author_printer.print_quotes()
+        author_printer.print()
 
     def pprint_pages(self, pages_scraper):
         author_printer = AuthorPrinter(pages_scraper)
-        author_printer.pprint_quotes()
+        author_printer.pprint()
 
     def print_quotes(self, page_scraper):
         quote_printer = QuotePrinter(page_scraper)
