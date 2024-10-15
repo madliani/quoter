3256554576343b5c8b9e571adf39eb1c525788ff
madliani <madliani@hotmail.com>
2024-10-15 16:02:16 +0400

    fix: fix author printer

--- src/wikiquote/printer/author_printer.py
+++ src/wikiquote/printer/author_printer.py
@@ -3,18 +3,18 @@ from urllib.parse import unquote
 
 class AuthorPrinter:
     def __init__(self, pages_scraper):
-        self.pages_scraper = pages_scraper
+        self.author_scraper = pages_scraper
 
     def print_all(self):
-        page_count = self.pages_scraper.page_count()
-        print(f"Список ссылок на персоналии ({page_count}): ")
+        author_count = self.author_scraper.get_count()
+        print(f"Список ссылок на персоналии ({author_count}): ")
 
-        for page in self.pages_scraper.all_pages():
+        for page in self.author_scraper.get_authors():
             print(f"https://ru.wikiquote.org/wiki{page}")
 
     def pprint_all(self):
-        page_count = self.pages_scraper.page_count()
-        print(f"Список ссылок на персоналии ({page_count}): ")
+        author_count = self.author_scraper.get_count()
+        print(f"Список ссылок на персоналии ({author_count}): ")
 
-        for page in self.pages_scraper.all_pages():
+        for page in self.author_scraper.get_authors():
             print(f"https://ru.wikiquote.org/wiki{unquote(page)}")
