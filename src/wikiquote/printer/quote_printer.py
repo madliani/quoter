42d23507379939a9fd938af05206619467b3eaa3
madliani <madliani@hotmail.com>
2024-10-15 15:57:54 +0400

    fix: fix quote printer

--- src/wikiquote/printer/quote_printer.py
+++ src/wikiquote/printer/quote_printer.py
@@ -1,12 +1,12 @@
 class QuotePrinter:
     def __init__(self, page_scraper):
-        self.page_scraper = page_scraper
+        self.quote_scraper = page_scraper
 
     def print_all(self):
-        author = self.page_scraper.get_author()
-        quote_count = self.page_scraper.get_count()
+        author = self.quote_scraper.get_author()
+        quote_count = self.quote_scraper.get_count()
         print(f"Список цитат ({author}) ({quote_count}):")
         print()
 
-        for quote in self.page_scraper.get_quotes():
+        for quote in self.quote_scraper.get_quotes():
             print(quote)
