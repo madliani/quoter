0b9a41eb9a6735c17c31d28a15498d2b75cd8cb0
madliani <madliani@hotmail.com>
2024-10-18 00:23:31 +0400

    refactor: refactor wikiquote scraper

--- src/wikiquote/scraper/wikiquote_scraper.py
+++ src/wikiquote/scraper/wikiquote_scraper.py
@@ -1,6 +1,3 @@
-import os
-from concurrent.futures import ThreadPoolExecutor
-
 from wikiquote.config.pages_config import PagesConfig
 from wikiquote.scraper.author_scraper import AuthorScraper
 from wikiquote.scraper.quote_scraper import QuoteScraper
@@ -8,12 +5,8 @@ from wikiquote.scraper.quote_scraper import QuoteScraper
 
 class WikiquoteScraper:
     def scrape_authors(self):
-        cpu_count = os.cpu_count()
         author_scraper = AuthorScraper(PagesConfig)
-
-        with ThreadPoolExecutor(max_workers=cpu_count) as thread_pool:
-            scrape_future = thread_pool.submit(author_scraper.scrape)
-            scrape_future.result()
+        author_scraper.scrape()
 
         return author_scraper
 
