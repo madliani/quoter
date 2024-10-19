47a9f2e202ea861eb37249a05660691ce620ea94
madliani <madliani@hotmail.com>
2024-10-19 17:24:37 +0400

    refactor: refactor painter font config

--- src/painter/configs/font_config.py
+++ src/painter/configs/font_config.py
@@ -1,7 +1,8 @@
-import os
 from enum import Enum
+
+from const.app_paths import AppPaths
 
 
 class FontConfig(Enum):
-    PATH = f"{os.getcwd()}/assets/fonts/LXGWWenKaiTC-Regular.ttf"
+    PATH = f"{AppPaths.APP_DIR.value}/assets/fonts/LXGWWenKaiTC-Regular.ttf"
     SIZE = 15
