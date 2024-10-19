a47fce23568130e6a7cf08b0cfc82e8a61ecb449
madliani <madliani@hotmail.com>
2024-10-19 16:59:43 +0400

    refactor: replace silero_tts by silero-tts

--- src/silero_tts/config/audio_config.py
+++
@@ -1,6 +0,0 @@
-import os
-from enum import Enum
-
-
-class AudioConfig(Enum):
-    FILENAME = f"{os.path.expanduser("~")}/Downloads/tts.wav"
--- src/silero_tts/config/speaker_config.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class SpeakerConfig(Enum):
-    SAMPLE_RATE = 48_000
-    SPEAKER = "baya"
