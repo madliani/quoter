0b47c41ecc9c09d24588e6ac86951e98c030bab9
madliani <madliani@hotmail.com>
2024-10-20 03:54:26 +0400

    feat: add wrapping a text to painter

--- src/painter/painter.py
+++ src/painter/painter.py
@@ -1,3 +1,4 @@
+import nltk
 from PIL import Image, ImageDraw, ImageFont
 
 from painter.configs.font_config import FontConfig
@@ -12,12 +13,29 @@ class Painter:
                                    size, ImageConfig.COLOR.value)
         self.__draw__ = ImageDraw.Draw(self.__image__)
 
+    def __wrap_text__(self, text, count):
+        tokens = nltk.word_tokenize(text)
+        splitted_tokens = []
+        wrapped_tokens = []
+
+        for i in range(0, len(tokens), count):
+            splitted_tokens.append(tokens[i:i + count])
+
+        for splitted_token in splitted_tokens:
+            wrapped_tokens.extend(splitted_token)
+            wrapped_tokens.append("\n")
+
+        return " ".join(wrapped_tokens)
+
     def write_text(self, text):
+        texts = self.__wrap_text__(text, 5)
         font = ImageFont.truetype(FontConfig.PATH.value, FontConfig.SIZE.value)
-        width, height = self.__draw__.textsize(text, font)
-        x = (ImageConfig.WIDTH.value - width) / 2
-        y = (ImageConfig.HEIGHT.value - height) / 2
-        self.__draw__.text((x, y), text, TextConfig.COLOR, font)
+
+        for i, text in enumerate(texts):
+            width, height = self.__draw__.textsize(text, font)
+            x = (ImageConfig.WIDTH.value - width) / 2
+            y = (ImageConfig.HEIGHT.value - height) / 2 + i * 10
+            self.__draw__.text((x, y), text, TextConfig.COLOR, font)
 
     def save(self, path):
         self.__image__.save(path)
