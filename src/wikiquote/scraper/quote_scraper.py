117063fe796577b2354c1d874f1fa6233bd11f41
madliani <madliani@hotmail.com>
2024-10-23 17:35:46 +0400

    fix: fix quoter scraper

--- src/wikiquote/scraper/quote_scraper.py
+++ src/wikiquote/scraper/quote_scraper.py
@@ -8,12 +8,12 @@ from wikiquote.const.exception_message import ExceptionMessage
 
 class QuoteScraper:
     def __init__(self, url):
-        self.__url__ = url
-        self.__author__ = None
-        self.__quotes__ = []
+        self._url_ = url
+        self._author_ = None
+        self._quotes_ = []
 
     def fetch(self):
-        response = requests.get(self.__url__, timeout=1)
+        response = requests.get(self._url_, timeout=1)
         response.raise_for_status()
 
         return response.text
@@ -22,38 +22,38 @@ class QuoteScraper:
         soup = BeautifulSoup(html, "lxml")
         title = soup.select_one("span.mw-page-title-main")
         quote_elements = soup.select("div.poem p")
-        self.__author__ = title.text
+        self._author_ = title.text
 
         for quote_elem in quote_elements:
-            self.__quotes__.append(quote_elem.text)
+            self._quotes_.append(quote_elem.text)
 
     def scrape(self):
         html = self.fetch()
         self.parse(html)
 
     def quotes(self):
-        if len(self.__quotes__) == 0:
+        if len(self._quotes_) == 0:
             raise Exception(ExceptionMessage.NO_QUOTES)
 
-        yield from self.__quotes__
+        yield from self._quotes_
 
     def quote(self, index):
-        if len(self.__quotes__) == 0:
+        if len(self._quotes_) == 0:
             raise Exception(ExceptionMessage.NO_QUOTES)
 
-        return self.__quotes__[index]
+        return self._quotes_[index]
 
     def author(self):
-        if self.__author__ is None:
+        if self._author_ is None:
             raise Exception(ExceptionMessage.NO_AUTHOR)
 
-        return self.__author__
+        return self._author_
 
     def random_quote(self):
-        if len(self.__quotes__) == 0:
+        if len(self._quotes_) == 0:
             raise Exception(ExceptionMessage.NO_QUOTES)
 
-        return random.choice(self.__quotes__)
+        return random.choice(self._quotes_)
 
     def length(self):
-        return len(self.__quotes__)
+        return len(self._quotes_)
