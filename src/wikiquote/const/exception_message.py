c24c3137b235f1e676e26b9cf71d63d2471fc0a8
madliani <madliani@hotmail.com>
2024-10-28 18:17:14 +0400

    feat: delete exception messages

--- src/wikiquote/const/exception_message.py
+++
@@ -1,8 +0,0 @@
-from enum import StrEnum
-
-
-class ExceptionMessage(StrEnum):
-    NO_AUTHOR = "No author!"
-    NO_AUTHORS = "No authors!"
-    NO_QUOTE = "No quote!"
-    NO_QUOTES = "No quotes!"
