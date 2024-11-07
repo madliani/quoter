46491ea9b43b0921a1478b74c331c52b80a24163
madliani <madliani@hotmail.com>
2024-11-07 18:58:25 +0400

    fix: fix author link scraper

--- src/scraper/author_link_scraper.py
+++ src/scraper/author_link_scraper.py
@@ -8,11 +8,11 @@ from config.author_link_config import AuthorLinkConfig
 
 class AuthorLinkScraper:
     def __init__(self: Self, config: AuthorLinkConfig) -> None:
-        self._url_ = config.URL
-        self._category_word_ = config.CATEGORY_WORD
+        self.__url = config.URL
+        self.__category_word = config.CATEGORY_WORD
 
     def fetch(self: Self) -> str:
-        response = requests.get(self._url_, timeout=1)
+        response = requests.get(self.__url, timeout=1)
         response.raise_for_status()
 
         return response.text
@@ -25,7 +25,7 @@ class AuthorLinkScraper:
         for author_link_elem in author_link_elements:
             author_link = author_link_elem.get("href")
 
-            if author_link and self._category_word_ not in author_link:
+            if author_link and self.__category_word not in author_link:
                 author_links.append(author_link)
 
         return author_links
