e0f1634a50f939a757be4382544f40a645a879a0
madliani <madliani@hotmail.com>
2024-11-07 19:03:31 +0400

    fix: fix random quote scraper types

--- src/scraper/random_quote_scraper.py
+++ src/scraper/random_quote_scraper.py
@@ -1,5 +1,5 @@
 import random
-from typing import Self
+from typing import Final, Self
 
 from config.author_link_config import AuthorLinkConfig
 from config.quote_config import QuoteConfig
@@ -9,8 +9,8 @@ from scraper.quote_scraper import QuoteScraper
 
 class RandomQuoteScraper:
     def __init__(self: Self) -> None:
-        self.__author_link_scraper = AuthorLinkScraper(AuthorLinkConfig)
-        self.__quote_scraper = QuoteScraper(QuoteConfig)
+        self.__author_link_scraper: Final = AuthorLinkScraper(AuthorLinkConfig)
+        self.__quote_scraper: Final = QuoteScraper(QuoteConfig)
 
     def quote_with_author(self: Self) -> str:
         author_links = self.__author_link_scraper.scrape()
