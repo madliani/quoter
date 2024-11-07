5e21ab1786841b1edae0ea2ea885d68bd1d1a3c6
madliani <madliani@hotmail.com>
2024-11-07 18:39:14 +0400

    feat: add types to random quote scraper

--- src/scraper/random_quote_scraper.py
+++ src/scraper/random_quote_scraper.py
@@ -1,4 +1,5 @@
 import random
+from typing import Self
 
 from config.author_link_config import AuthorLinkConfig
 from config.quote_config import QuoteConfig
@@ -7,11 +8,11 @@ from scraper.quote_scraper import QuoteScraper
 
 
 class RandomQuoteScraper:
-    def __init__(self):
+    def __init__(self: Self) -> None:
         self.author_link_scraper = AuthorLinkScraper(AuthorLinkConfig)
         self.quote_scraper = QuoteScraper(QuoteConfig)
 
-    def quote_with_author(self):
+    def quote_with_author(self: Self) -> str:
         author_links = self.author_link_scraper.scrape()
         author_link = random.choice(author_links)
         (author, quotes) = self.quote_scraper.scrape(author_link)
