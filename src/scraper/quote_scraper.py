63328c6ff0cb22c4b86a1c527692fdf9f50d0c9b
madliani <madliani@hotmail.com>
2024-12-02 14:00:51 +0400

    fix: fix quote scraper types

--- src/scraper/quote_scraper.py
+++ src/scraper/quote_scraper.py
@@ -11,7 +11,7 @@ class QuoteScraper:
         self.__base_url: Final = config.BASE_URL
 
     def __fetch(self: Self, url: str):
-        response: Final = requests.get(url, timeout=1)
+        response = requests.get(url, timeout=1)
         response.raise_for_status()
 
         return response.text
