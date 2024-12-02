a43d360dc2e10a7a88e4e305d09c32743584d002
madliani <madliani@hotmail.com>
2024-12-02 13:34:22 +0400

    fix: fix quote scraper types

--- src/scraper/quote_scraper.py
+++ src/scraper/quote_scraper.py
@@ -5,18 +5,20 @@ from bs4 import BeautifulSoup
 
 from config.quote_config import QuoteConfig
 
+type ParseResult = (Final[str], Final[list[str]])
+
 
 class QuoteScraper:
     def __init__(self: Self, config: QuoteConfig) -> None:
         self.__base_url: Final = config.BASE_URL
 
-    def __fetch(self: Self, url: str) -> str:
+    def __fetch(self: Self, url: str) -> Final[str]:
         response = requests.get(url, timeout=1)
         response.raise_for_status()
 
         return response.text
 
-    def __parse(self: Self, html: str) -> (str, list[str]):
+    def __parse(self: Self, html: str) -> ParseResult:
         soup = BeautifulSoup(html, "lxml")
         title = soup.select_one("span.mw-page-title-main")
         quote_elements = soup.select("div.poem p")
@@ -28,7 +30,7 @@ class QuoteScraper:
 
         return (author, quotes)
 
-    def scrape(self: Self, author_link: str) -> (str, list[str]):
+    def scrape(self: Self, author_link: str) -> ParseResult:
         url = f"{self.__base_url}{author_link}"
         html = self.__fetch(url)
 
