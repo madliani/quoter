687f26640b2340d5c6a98b52295308d30cfd4b88
madliani <madliani@hotmail.com>
2024-12-02 14:00:10 +0400

    fix: fix author link scraper types

--- src/scraper/author_link_scraper.py
+++ src/scraper/author_link_scraper.py
@@ -11,13 +11,13 @@ class AuthorLinkScraper:
         self.__url: Final = config.URL
         self.__category_word: Final = config.CATEGORY_WORD
 
-    def __fetch(self: Self) -> str:
+    def __fetch(self: Self):
         response = requests.get(self.__url, timeout=1)
         response.raise_for_status()
 
         return response.text
 
-    def __parse(self: Self, html: str) -> list[str]:
+    def __parse(self: Self, html: str):
         soup = BeautifulSoup(html, "lxml")
         author_link_elements = soup.select("div.mw-category-group ul li a")
         author_links = []
@@ -30,7 +30,7 @@ class AuthorLinkScraper:
 
         return author_links
 
-    def scrape(self: Self) -> str:
+    def scrape(self: Self):
         html = self.__fetch()
 
         return self.__parse(html)
