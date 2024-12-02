ef6c48a2d2339c54bca0c9c12f9a60fa0b1bdfb4
madliani <madliani@hotmail.com>
2024-12-02 13:56:08 +0400

    fix: fix quote scraper types

--- src/scraper/quote_scraper.py
+++ src/scraper/quote_scraper.py
@@ -5,32 +5,30 @@ from bs4 import BeautifulSoup
 
 from config.quote_config import QuoteConfig
 
-type ParseResult = (str, Final[list[str]])
-
 
 class QuoteScraper:
     def __init__(self: Self, config: QuoteConfig) -> None:
         self.__base_url: Final = config.BASE_URL
 
-    def __fetch(self: Self, url: str) -> str:
+    def __fetch(self: Self, url: str):
         response: Final = requests.get(url, timeout=1)
         response.raise_for_status()
 
         return response.text
 
-    def __parse(self: Self, html: str) -> ParseResult:
-        soup: Final = BeautifulSoup(html, "lxml")
-        title: Final = soup.select_one("span.mw-page-title-main")
-        quote_elements: Final = soup.select("div.poem p")
-        author: Final = title.text
+    def __parse(self: Self, html: str):
+        soup = BeautifulSoup(html, "lxml")
+        title = soup.select_one("span.mw-page-title-main")
+        quote_elements = soup.select("div.poem p")
+        author = title.text
         quotes = []
 
         for quote_elem in quote_elements:
             quotes.append(quote_elem.text)
 
-        return (author, quotes)
+        return author, quotes
 
-    def scrape(self: Self, author_link: str) -> ParseResult:
+    def scrape(self: Self, author_link: str):
         url = f"{self.__base_url}{author_link}"
         html = self.__fetch(url)
 
